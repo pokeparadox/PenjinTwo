9b047221bef592c13f0042c85d74d84e5aea2de2
Kevin Winfield-Pantoja <kevin@pirategames.co.uk>
2011-05-29 09:52:23 -0400

    Added: Examples folder

---
+++ examples/MyStateChangeCode.h
@@ -0,0 +1,35 @@
+#ifndef MYSTATECHANGECODE_H_INCLUDED
+#define MYSTATECHANGECODE_H_INCLUDED
+
+/**
+	\section LICENSE
+	Penjin is Copyright (c)2005, 2006, 2007, 2008, 2009, 2010, 2011 Kevin Winfield-Pantoja
+
+	This file is part of Penjin.
+
+	Penjin is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Penjin is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with Penjin.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/***************************************************************************************************
+*   \file MyStateChangeCode is your custom code to handle state changes within the StateManager
+*   \author Kevin Winfield-Pantoja
+*/
+
+case STATE_TITLE:
+{
+    state = new StateTitle;
+    break;
+}
+
+
+#endif // MYSTATECHANGECODE_H_INCLUDED
---
+++ examples/MyStateIncludes.h
@@ -0,0 +1,31 @@
+#ifndef MYSTATEINCLUDES_H_INCLUDED
+#define MYSTATEINCLUDES_H_INCLUDED
+
+/**
+	\section LICENSE
+	Penjin is Copyright (c)2005, 2006, 2007, 2008, 2009, 2010, 2011 Kevin Winfield-Pantoja
+
+	This file is part of Penjin.
+
+	Penjin is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Penjin is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with Penjin.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/***************************************************************************************************
+*   \file MyStateIncludes is a simple list of the state headers required for this specific project.
+*   \author Kevin Winfield-Pantoja
+*/
+
+// List all state headers below
+#include "StateTitle.h"
+
+#endif // MYSTATEINCLUDES_H_INCLUDED
