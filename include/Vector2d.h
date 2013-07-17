ef72d162e953f4b52545d739f5dd01b95539eef7
PokeParadox <pokeparadox@gmail.com>
2013-07-17 22:07:17 +0100

    Added division with assign operators

--- include/Vector2d.h
+++ include/Vector2d.h
@@ -97,11 +97,17 @@ namespace Penjin
             Vector2d<T> operator/(const T& v)const{return Vector2d<T>(this->x / v, y / v);}
             template <class S>
             Vector2d<T> operator/(const S& v)const{return Vector2d<T>((this->x / v)+0.5f, (y / v)+0.5f);}
-            // Multiplay and assign
+            // Multiply and assign
             Vector2d<T> operator*=(const Vector2d<T> & v){return Vector2d<T> (this->x *= v.x, y *= v.y);}
             template <class S>
             Vector2d<T> operator*=(const Vector2d<S> & v){return Vector2d<T> (this->x *= v.x, y *= v.y);}
             Vector2d<T> operator*=(const T& v){return Vector2d<T>(this->x *= v, y *= v);}
+
+            // Divideand assign
+            Vector2d<T> operator/=(const Vector2d<T> & v){return Vector2d<T> (this->x /= v.x, y /= v.y);}
+            template <class S>
+            Vector2d<T> operator/=(const Vector2d<S> & v){return Vector2d<T> (this->x /= v.x, y /= v.y);}
+            Vector2d<T> operator/=(const T& v){return Vector2d<T>(this->x /= v, y /= v);}
         T y;
     };
 
