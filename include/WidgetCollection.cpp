850e102266aa7952545d1a6e17f6198eb3f5962e
PokeParadox <pokeparadox@gmail.com>
2013-07-30 21:34:10 +0100

    Added WidgetCollection
    Added WidgetCollection which simply acts as a base container for
    Widgets

---
+++ include/WidgetCollection.cpp
@@ -0,0 +1,51 @@
+#include "WidgetCollection.h"
+#include "Widget.h"
+using Penjin::WidgetCollection;
+using Penjin::Widget;
+WidgetCollection::WidgetCollection()
+{
+    //ctor
+}
+
+WidgetCollection::~WidgetCollection()
+{
+    //dtor
+     clear();
+}
+
+
+void WidgetCollection::clear()
+{
+    for(int i = widgets.size()-1; i>=0; --i)
+    {
+        delete widgets.at(i);
+        widgets.at(i) = NULL;
+    }
+    widgets.clear();
+}
+
+void WidgetCollection::add(Widget* w)
+{
+    if(w != NULL)
+        widgets.push_back(w);
+}
+
+Widget* WidgetCollection::getWidest()
+{
+    int width = 0;
+    int index = 0;
+    for(int i = size()-1; i >= 0;--i)
+    {
+        if(widgets.at(i)->getWidth() > width)
+        {
+            index = i;
+            width = widgets.at(i)->getWidth();
+        }
+    }
+    return widgets.at(index);
+}
+
+Widget* WidgetCollection::getTallest()
+{
+
+}
---
+++ include/WidgetCollection.h
@@ -0,0 +1,37 @@
+#ifndef WIDGETCOLLECTION_H
+#define WIDGETCOLLECTION_H
+
+#include "Widget.h"
+namespace Penjin
+{
+    class WidgetCollection : public Widget
+    {
+        public:
+            /** Default constructor */
+            WidgetCollection();
+            /** Default destructor */
+            virtual ~WidgetCollection();
+
+                        // add a configured widget to the Panel
+            void add(Widget* w);
+            void edit(const unsigned int& i, Widget* w);
+            void remove(const unsigned int& i);
+            Widget* at(const unsigned int& i);
+
+            Widget* getWidest();
+            Widget* getTallest();
+
+            /// Size operations
+            size_t size()const{return widgets.size();}
+            bool empty()const{return !size();}
+            void clear();
+
+        protected:
+            /** We store all our widget pointers here**/
+            vector<Widget*> widgets;
+    };
+
+}
+
+
+#endif // WIDGETCOLLECTION_H
